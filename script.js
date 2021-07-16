a8fa7ca0dec5b7e7b9f8f0c5e015f008a1597852
lucas vital <lucasvitaldemelo@gmail.com>
2021-07-16 17:42:41 -0300

    .

--- script.js
+++ script.js
@@ -41,15 +41,35 @@ const transactions = [
 ]
 
 const Transaction = {
-    incomes() {
-        //somar as entradas
+    all: transactions,
+
+    add(transaction){
+        Transaction.all.push(transaction)
+
+        App.reload()
     },
+    incomes() {
+        let income = 0;
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount;
+            }
+        })
+        return income;
+    },
+
     expenses() {
-        //somar as saídas
+        let expense = 0;
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount;
+            }
+        })
+        return expense;
     },
 
     total() {
-        //entrada - saídas
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
@@ -65,11 +85,11 @@ const DOM = {
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
-        const amount = transaction.
+        const amount = Ultils.formatCurrency(transaction.amount)
 
         const html = `
         <td class="description">${transaction.description}</td>
-        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
             <img src="./assets/minus.svg" alt="Remover trasação">
@@ -77,11 +97,53 @@ const DOM = {
        `
 
        return html
+    },
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Ultils.formatCurrency(Transaction.incomes())
+        document
+            .getElementById('expenseDisplay')
+                .innerHTML = Ultils.formatCurrency(Transaction.expenses())
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Ultils.formatCurrency(Transaction.total())
+
+    },
+
+    clearTransaction(){
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
-const Ultils
+const Ultils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
 
-transactions.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
-})
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: 'currency',
+            currency: 'BRL'
+        })
+        return signal + value
+    }
+}
+
+const App = {
+    init() {
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+    },
+    reload(){
+        DOM.clearTransaction()
+        App.init()  
+    },
+}
+
+App.init()
